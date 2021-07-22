b398edd3f347465b22f6b4c04ddd787de15f5a60
Lulu-Soso <lucas.sokha@gmail.com>
2021-07-22 17:12:02 +0200

    created SearchType form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,13 +23,25 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         // récupère tous les produits à l'aide de l'ORM doctrine
         $products = $this->entityManager->getRepository(Product::class)->findAll();
 
+        // On passe l'instance de la class Search dans le formulaire SearchType  
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        // methode qui permet d'écouter si le formulaire a été posté
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
+
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Classe\Search;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -18,6 +19,33 @@ class ProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Product::class);
     }
+
+    /**
+     * Requete qui me permet de récupérer les produits en fonction de la recherche de l'utilisateur
+     * @return Product[]
+     */
+    public function findWithSearch(Search $search)
+    {
+        // On crée une requête avec la variable $query et à l'intérieur, on va utiliser plusieurs méthodes
+        $query = $this
+            ->createQueryBuilder('p') // faire le mapping avec la table 'p' comme product
+            ->select('c', 'p')        // 'c' comme category
+            ->join('p.category', 'c'); // une jointure
+
+
+        // Uniquement et lorsque les catégories ont été coché, je voudrais que tu rajoutes 
+        // dans cette requête un WHERE qui me permet de filtrer mes catégories.
+        if (!empty($search->categories)) {
+            $query = $query
+                ->andWhere('c.id IN (:categories)') // besoin que les Id de mes categories soient dans la liste des categories envoyée en paramètre dans l'objet $search
+                ->setParameter('categories', $search->categories); // donner un paramètre qui aura le nom categories, et sa valeur, c'est qu'il y a dans $search->categories
+        }
+
+        // je veux que tu me retournes la query, que tu l'executes et la crée, et que tu me retournes les resultats
+        return $query->getQuery()->getResult();
+    }
+
+
 
     // /**
     //  * @return Product[] Returns an array of Product objects
